25b5f93991f114e9dd8f0b101b2a9241f2bf3ec5
alexxis8 <yo.alexis08@gmail.com>
2023-12-06 13:55:42 -0300

    barra de busqueda

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,5 +1,7 @@
 const bcryptjs = require('bcryptjs');
 const db = require('../database/models');
+const { Sequelize } = require("../database/models");
+const Op = Sequelize.Op;
 
 const mainController = {
   home: (req, res) => {
@@ -12,15 +14,38 @@ const mainController = {
       .catch((error) => console.log(error));
   },
   bookDetail: (req, res) => {
-    // Implement look for details in the database
-    res.render('bookDetail');
+    // Implement look for details in the database 
+    const id = req.params.id;
+    db.Book.findByPk(id, {
+      include: [{ association: "authors" }],
+    })
+      .then((book) => {
+        res.render("bookDetail", {book});
+      })
+      .catch((error) => console.log(error));             
   },
   bookSearch: (req, res) => {
     res.render('search', { books: [] });
   },
   bookSearchResult: (req, res) => {
     // Implement search by title
-    res.render('search');
+    const title = req.body.title;
+    console.log(title);
+    if (title.length == 0) {
+      res.render("search", { books: []});
+    }
+    db.Book.findAll({
+      include: [{ association: "authors" }],
+      where: {
+        title: {
+          [Op.like]: `%${title}%`,
+        },
+      },
+    })
+      .then((books) => {
+        res.render("search", { books});
+      })
+      .catch((error) => console.log(error));
   },
   deleteBook: (req, res) => {
     // Implement delete book
